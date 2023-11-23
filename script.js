36b6dc7b93dae165a52a5405784b2c5fa789939f
ilya <iliprogramist2006@gmail.com>
2023-11-23 19:31:45 +0300

    did виды транспорта

---
+++ script.js
@@ -0,0 +1,28 @@
+let carImages = document.querySelectorAll(".vehicle-1");
+
+
+carImages.forEach(function (image) {
+    image.addEventListener("click", function () {
+        carImages.forEach(function (img) {
+            img.classList.remove("carImg");
+        });
+        image.classList.add("carImg");
+    });
+});
+
+
+let mainImg = document.querySelector(".main-image");
+let descrTitle = document.querySelector(".description-title");
+let descrText = document.querySelector(".description-text");
+
+function showImg(pathImg, title){
+    if(pathImg != "./vehicles/01.jpg"){
+        descrTitle.innerHTML = title;
+        descrText.innerHTML = "Полноцветная печать на плёнке Avery 3000";
+    } else {
+        descrTitle.innerHTML = "";
+        descrText.innerHTML = "";
+    };
+    mainImg.src = pathImg;
+};
+ 
