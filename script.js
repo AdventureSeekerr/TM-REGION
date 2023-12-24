e899c20c53f65e5899b4b8db56baaf69912622ba
ilya bondarev <proghtmll@gmail.com>
2023-12-24 23:47:23 +0300

    did animation map

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ let mainImg = document.querySelector(".main-image");
 let descrTitle = document.querySelector(".description-title");
 let descrText = document.querySelector(".description-text");
 
-function showImg(pathImg, title){
-    if(pathImg != "./vehicles/01.jpg"){
+function showImg(pathImg, title) {
+    if (pathImg != "./vehicles/01.jpg") {
         descrTitle.innerHTML = title;
         descrText.innerHTML = "Полноцветная печать на плёнке Avery 3000";
     } else {
@@ -34,19 +34,20 @@ const urFO = document.querySelectorAll(".cls-ur");
 const sbFO = document.querySelectorAll(".cls-sb");
 const dalFO = document.querySelectorAll(".cls-dal");
 
-function showRegion(array){
-    array.forEach(function(elements){
+function showRegion(array) {
+    array.forEach(function (elements) {
         elements.addEventListener("mouseover", () => {
-            array.forEach(function(el){
+            array.forEach(function (el) {
                 el.style.fill = "#da0000"
             });
         });
-        elements.addEventListener("mouseout", ()=>{
-            array.forEach(function(el){
-                el.style.fill = "#828181"
+        elements.addEventListener("mouseout", () => {
+            array.forEach(function (el) {
+                el.style.fill = "#c1c1c1"
             });
         });
     });
+
 };
 showRegion(szFO);
 showRegion(centrFO);
@@ -57,4 +58,76 @@ showRegion(urFO);
 showRegion(sbFO);
 showRegion(dalFO);
 
+const onlySzFO = [...centrFO, ...privFO, ...usnFO, ...skFO, ...urFO, ...sbFO, ...dalFO];
+const onlyCentrFO = [...szFO, ...privFO, ...usnFO, ...skFO, ...urFO, ...sbFO, ...dalFO];
+const onlyPrivFO = [...szFO, ...centrFO, ...usnFO, ...skFO, ...urFO, ...sbFO, ...dalFO];
+const onlyUsnFO = [...szFO, ...centrFO, ...privFO, ...skFO, ...urFO, ...sbFO, ...dalFO];
+const onlySkFO = [...szFO, ...centrFO, ...privFO, ...usnFO, ...urFO, ...sbFO, ...dalFO];
+const onlyUrFO = [...szFO, ...centrFO, ...privFO, ...usnFO, ...skFO, ...sbFO, ...dalFO];
+const onlySbFO = [...szFO, ...centrFO, ...privFO, ...usnFO, ...skFO, ...urFO, ...dalFO];
+const onlyDalFO = [...szFO, ...centrFO, ...privFO, ...usnFO, ...skFO, ...urFO, ...sbFO];
+
+
+const infoSz = document.querySelector(".info-region__sz");
+const infoCen = document.querySelector(".info-region__cen");
+const infoUsn = document.querySelector(".info-region__usn");
+const infoSk = document.querySelector(".info-region__sk");
+const infoPriv = document.querySelector(".info-region__priv");
+const infoUr = document.querySelector(".info-region__ur");
+const infoSib = document.querySelector(".info-region__sib");
+const infoDal = document.querySelector(".info-region__dal");
+const sloi1 = document.querySelector("#Слой_1");
+
+function removeRegions(region, regionArray, descrRegion, transition) {
+    region.forEach((el) => {
+        el.addEventListener("click", () => {
+            region.forEach((animation) => {
+                animation.classList.add(transition);
+                setTimeout(() => {
+                    if (animation.classList.contains(transition)) {
+                        document.addEventListener("click", () => {
+                            animation.classList.remove(transition);
+                        })
+                    } else{
+                        animation.classList.add(transition);
+                    }
+                }, 0.3)
+            });
+            sloi1.style.height = "500px";
+            regionArray.forEach((regions) => {
+                regions.style.display = "none";
+                setTimeout(() => {
+                    if (regions.style.display = "none") {
+                        document.addEventListener("click", () => {
+                            regions.style.display = "block";
+                        })
+                    } else{
+                        regions.style.display = "none";
+                    }
+                }, 0.3)
+            });
+            descrRegion.classList.add("info-region__active");
+            setTimeout(() => {
+                if (descrRegion.classList.contains("info-region__active")) {
+                    document.addEventListener("click", () => {
+                        descrRegion.classList.remove("info-region__active");
+                    })
+                } else {
+                    descrRegion.classList.add("info-region__active");
+                }
+            }, 0.3)
+        });
+    });
+}
+
+
+
+removeRegions(szFO, onlySzFO, infoSz, "info-region__transition");
+removeRegions(centrFO, onlyCentrFO, infoCen, "info-region__transition-2");
+removeRegions(usnFO, onlyUsnFO, infoUsn, "info-region__transition-3");
+removeRegions(skFO, onlySkFO, infoSk, "info-region__transition-4");
+removeRegions(privFO, onlyPrivFO, infoPriv, "info-region__transition-5");
+removeRegions(urFO, onlyUrFO, infoUr, "info-region__transition-6");
+removeRegions(sbFO, onlySbFO, infoSib, "info-region__transition-7");
+removeRegions(dalFO, onlyDalFO, infoDal, "info-region__transition-8");
 
